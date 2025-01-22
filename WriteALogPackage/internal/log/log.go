3a53c789ee71e735b4d27aaff5e0d8dfb1a9a1af
unknown <the2sang@gmail.com>
2025-01-22 16:49:27 +0900

    6th commit

--- WriteALogPackage/internal/log/log.go
+++ WriteALogPackage/internal/log/log.go
@@ -1,7 +1,6 @@
 package log
 
 import (
-	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
@@ -96,6 +95,7 @@ func (l *Log) Read(off uint64) (*api.Record, error) {
 	}
 	if s == nil || s.nextOffset <= off {
 		return nil, api.ErrOffsetOutOfRange{Offset: off}
+
 	}
 	return s.Read(off)
 }
